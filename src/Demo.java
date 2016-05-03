612b7dd3602417cb63337e6417c07175dac3249a
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-03 14:53:09 +0300

    Add 4 lines of code to test rebase with conflict

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,6 @@
  */
 public class Demo {
     public static void main(String[] args) {
-
         System.out.println("Hello");
         System.out.println();
         System.out.println("Boom you dead");
