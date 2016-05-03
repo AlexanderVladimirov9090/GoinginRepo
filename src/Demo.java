2571a3568009d0d8e24a36f0ae48e28fea9a5def
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-03 14:54:10 +0300

    Remove Lines to try rebase with conflict

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println();
-        System.out.println("Boom you dead");
+
     }
 }
